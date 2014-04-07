151e46dd6596a9233e2c4d20d0f8829dec478fb1
Christophe Hoch <christophe.hoch@gmail.com>
2014-04-07 23:37:09 +0200

    Travail sur les slides

--- js/main.js
+++ js/main.js
@@ -5,18 +5,4 @@ Reveal.initialize({
     progress: true,
     history: true,
     center: true
-
-    //theme: Reveal.getQueryHash().theme, // available themes are in /css/theme
-    //transition: Reveal.getQueryHash().transition || 'default', // default/cube/page/concave/zoom/linear/fade/none
-
-    // Parallax scrolling
-    // parallaxBackgroundImage: 'https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg',
-    // parallaxBackgroundSize: '2100px 900px',
-
-    // Optional libraries used to extend on reveal.js
-    //dependencies: [
-    //    { src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
-    //    { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
-    //    { src: 'plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
-    //]
 });
